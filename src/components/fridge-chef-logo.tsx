18e8493a6e65f17a7d3ed1f7cd8962322423c792
CrYpTo KiNgS <cryptlite12@gmail.com>
2025-04-10 04:39:22 +0000

    have gradient colors for FridgeChef wording

--- src/components/fridge-chef-logo.tsx
+++ src/components/fridge-chef-logo.tsx
@@ -23,7 +23,7 @@ export const FridgeChefLogo = React.forwardRef<
           clipRule="evenodd"
         />
       </svg>
-      <span>FridgeChef</span>
+      <span className="bg-gradient-to-r from-primary to-accent text-white bg-clip-text text-3xl font-bold text-transparent">FridgeChef</span>
     </h1>
   );
 });
